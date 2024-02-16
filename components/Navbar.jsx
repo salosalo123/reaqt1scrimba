63411fed8ccb90daf2fb18f2f2b022c1bad1a778
Salome Gogritchiani <beruchashvilitamar@gmail.com>
2024-02-16 15:17:56 +0400

    css

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,10 @@
 import React from "react";
 export default function Navbar() {
-  return <h1> NAvbar component</h1>;
+  return (
+    <nav>
+      <img src="../img/images.png" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React course - Project 1</h4>
+    </nav>
+  );
 }
