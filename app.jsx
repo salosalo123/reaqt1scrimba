a3f1e25d275276e3f2f8e22ef2c0dd530f11e357
Salome Gogritchiani <beruchashvilitamar@gmail.com>
2024-02-16 12:13:45 +0400

    quiz

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from "reaact";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <div className="conteiner">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
