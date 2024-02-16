8acaf422cc6b1676130d203c81c08ad15d0a4944
Salome Gogritchiani <beruchashvilitamar@gmail.com>
2024-02-16 15:36:32 +0400

    css

---
+++ Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../img/images.png" />
+      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h4 className="nav--title">React course - Project 1</h4>
+    </nav>
+  );
+}
--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,15 @@
 import React from "react";
 export default function Main() {
-  return <h1> Main component</h1>;
+  return (
+    <main>
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li> Was first released in 2023 </li>
+        <li>Was originally created by Jordan Walker</li>
+        <li>HAs well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobie apps</li>
+      </ul>
+    </main>
+  );
 }
